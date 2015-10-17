ce0d08f255aaa17dd8288aca229cad1cf33b57f5
Lukas Höppner <lukas.hoeppner@gmail.com>
2015-10-17 13:28:31 +0200

    working example if deleting all libraries from lib/

--- src/esp_main.cpp
+++ src/esp_main.cpp
@@ -1,9 +1,27 @@
-#include <raftNode.h>
+
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <WiFiClient.h>
+#include <WiFiServer.h>
+#include <WiFiUdp.h>
+
+const unsigned int MAX_MESSAGE_SIZE = 512;
+
+WiFiUDP udp;
+IPAddress ip(192,168,178,31);
+byte messageBuffer[MAX_MESSAGE_SIZE];
 
 void setup() {
-
+  WiFi.mode(WIFI_STA);
+  WiFi.begin("", "");
 }
 
 void loop() {
-
+  udp.beginPacket(ip, 55056);
+  String m = "Hello, World!";
+  m.getBytes(messageBuffer, MAX_MESSAGE_SIZE);
+  udp.write(messageBuffer, m.length());
+  udp.endPacket();
+  delay(1000);
 }
